01371ef8683efb514710d254d6f3b715a1965564
Lorena Perales <lperales321@gmail.com>
2019-09-30 23:16:44 -0500

    Add switch statement to process commands

--- liri.js
+++ liri.js
@@ -1,3 +1,6 @@
+//Include axios npm package
+const axios = require('axios');
+
 //Read and Set Environment variables
 require("dotenv").config();
 
@@ -6,3 +9,45 @@ var keys = require("./keys.js");
 
 //Access Spotify keys
 var spotify = new Spotify(keys.spotify);
+
+//Get parameters and store it in a variable
+const command = process.argv[2];
+const search = process.argv[3];
+
+switch(command) {
+    case 'concert-this':
+        console.log("Search Concert");
+        searchConcert(search);
+        break;
+    case 'spotify-this-song':
+        console.log("Search Song");
+        searchSong(search);
+        break;
+    case 'movie-this':
+        console.log("Search Movie");
+        searchMovie(search);
+        break;
+    case 'do-what-it-says':
+        console.log("Search File");
+        searchFile(search);
+        break;
+}
+
+function searchMovie(movieName) {
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+
+
+    // Then create a request with axios to the queryUrl
+    // ...
+    axios
+        .get(queryUrl)
+        .then(function(response) {
+            console.log(response.data.Year);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+}
