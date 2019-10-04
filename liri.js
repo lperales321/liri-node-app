da45b218e58b435573aa4fa034a283d25a202792
Lorena Perales <lperales321@gmail.com>
2019-10-04 17:57:10 -0500

    Add logic to get concert information for an artist

--- liri.js
+++ liri.js
@@ -1,51 +1,101 @@
 //Include axios npm package
 const axios = require('axios');
+const moment = require('moment');
 
 //Read and Set Environment variables
 require("dotenv").config();
 
 //Import keys
-var keys = require("./keys.js");
+const keys = require("./keys.js");
 
 //Access Spotify keys
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
+
+const divider = "\n------------------------------------------------------------------------------------\n";
 
 //Get parameters and store it in a variable
 const command = process.argv[2];
 const search = process.argv[3];
 
+console.log(`Action: ${command}`);
+console.log(`Search: ${search}`);
+
 switch(command) {
     case 'concert-this':
-        console.log("Search Concert");
-        searchConcert(search);
+        let artist = search;
+        if(artist === undefined) {
+            artist = "Ace of Base"
+        }
+        searchConcert(artist);
         break;
+
     case 'spotify-this-song':
         console.log("Search Song");
         searchSong(search);
         break;
+
     case 'movie-this':
         console.log("Search Movie");
-        searchMovie(search);
+        let movie = search;
+        if(movie === undefined) {
+            movie = "Mr. Nobody"
+        }
+        searchMovie(movie);
         break;
+        
     case 'do-what-it-says':
         console.log("Search File");
         searchFile(search);
         break;
 }
 
-function searchMovie(movieName) {
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+function searchConcert(artist) {
+    let combineArtist = artist.split(' ').join('+');
 
-    // This line is just to help us debug against the actual URL.
-    console.log(queryUrl);
+    var queryUrl = "https://rest.bandsintown.com/artists/" + combineArtist + "/events?app_id=codingbootcamp";
 
-
-    // Then create a request with axios to the queryUrl
-    // ...
     axios
         .get(queryUrl)
         .then(function(response) {
-            console.log(response.data.Year);
+            let showData = "Concerts for: " + artist + "\n\n";
+
+            for (const concert of response.data) {
+                showData += [
+                    "Name of the Venue: " + concert.venue.name,
+                    "Venue Location: " + concert.venue.city + ", " + concert.venue.country,
+                    "Date of the Event: " + moment(concert.datetime).format("MM/DD/YYYY"),
+                    divider
+                ].join("\n\n");
+            }
+            
+            console.log(showData);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+}
+
+function searchMovie(movie) {
+    let combinedMovie = movie.split(' ').join('+');
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + combinedMovie + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
+
+    axios
+        .get(queryUrl)
+        .then(function(response) {
+            let showData = "Movie: " + movie + "\n\n";
+            console.log(response.data);
+            // for (const concert of response.data) {
+            //     showData += [
+            //         "Name of the Venue: " + concert.venue.name,
+            //         "Venue Location: " + concert.venue.city + ", " + concert.venue.country,
+            //         "Date of the Event: " + moment(concert.datetime).format("MM/DD/YYYY"),
+            //         divider
+            //     ].join("\n\n");
+            // }
+            
+            console.log(showData);
         })
         .catch(function(error) {
             console.log(error);
